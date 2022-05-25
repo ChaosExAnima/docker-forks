27a118ea4b20e412fa1cc6fb349ac20c7a1a0f82
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2022-05-24 22:33:59 -0400

    Switch to a Docker Compose based workflow.

--- build.mjs
+++ build.mjs
@@ -15,49 +15,43 @@ function showHelpAndExit() {
 	process.exit();
 }
 
-async function getEnvVars() {
-	const rawEnv = await fs.readFile( path.resolve( process.env.PWD, '.env' ), { encoding: 'UTF-8' } );
-	const envMap = new Map();
-	for ( const env of rawEnv.split( '\n' ) ) {
-		const [ key, value ] = env.trim().split( '=', 2 );
-		if ( ! key || ! value ) {
-			continue;
-		}
-		envMap.set( key, value );
-	}
-	return envMap;
-}
-
 /** @type string */
-const [ image ] = argv._.slice( 1 );
-/** @type string */
-const tag = argv.tag ?? 'latest';
+let [ image ] = argv._.slice( 1 );
 /** @type string[] */
-const images = ( await globby( './**/Dockerfile' ) )
-	.map( str => String( str ).trim().slice( 0, -11 ) );
-
-if ( ! image || ! images.includes( image ) ) {
-	showHelpAndExit();
-}
-
-/** @type Map<string, string> */
-const envs = await getEnvVars();
-const owner = envs.get( 'OWNER' );
-if ( ! owner ) {
-	console.log( 'Owner not set.' );
+let images = [];
+const composePath = path.resolve( __dirname, 'docker-compose.yml' );
+try {
+	/** @type string */
+	const composeFile = fs.readFileSync( composePath, 'utf-8' );
+	if ( ! composeFile ) {
+		throw new Error();
+	}
+	/** @type {services: {[key: string]: unknown}} */
+	const compose = YAML.parse( composeFile );
+	images = Object.keys( compose.services );
+} catch (err) {
+	console.log( `Could not find Compose file at ${ composePath }? 😕` );
 	process.exit( 1 );
 }
 
-console.log( chalk.bold( `Building image ${image}... 🔨` ) );
-await $`docker build -f ${ image }/Dockerfile . -t ${ owner }/${ image }:${ tag }`;
+if ( ! images.includes( image ) && image !== 'all' ) {
+	showHelpAndExit();
+}
+
+if ( image === 'all' ) {
+	image = '';
+}
+
+console.log( chalk.bold( `Building image ${ image }... 🔨` ) );
+await $`docker compose -f ${ composePath } build ${ image }`;
 
 const push = argv.push === 'true';
 if ( ! push ) {
-	console.log( chalk.bold( 'Thatʼs all, folks! 👋' ) );
+	console.log( chalk.bold( 'That\'s all, folks! 👋' ) );
 	process.exit();
 }
 
-console.log( chalk.bold( 'Pushing to Docker Hub...  ⬆🌥' ) );
-await $`docker push ${ owner }/${ image }:${ tag }`;
+console.log( chalk.bold( 'Pushing to Docker Hub... ⬆🌥' ) );
+await $`docker compose -f ${ composePath } push ${ image }`;
 
 console.log( chalk.bold( '💥🎤💧' ) );
